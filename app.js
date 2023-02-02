27cef07368982ac55082f7e8e2b662b9c4e21495
soicute <119741442+soinhocute@users.noreply.github.com>
2023-02-02 21:54:37 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+//const btn1 = document.getElementById("button1")
+//console.log(btn1)
+//const btn2 = document.getElementById("button2")
+//const btn3 = document.getElementById("button3")
+//const btn4 = document.getElementById("button4")
+//const btn5 = document.getElementById("button5")
+//const btn6 = document.getElementById("button6")
+
+const a = document.getElementById("name1")
+const b = document.getElementById("name2")
+const c = document.getElementById("name3")
+const d = document.getElementById("name4")
+const e = document.getElementById("name5")
+const f = document.getElementById("name6")
+
+function btn1() {
+    localStorage.setItem("Áo hoodie dài tay có mũ nỉ trơn unisex nam nữ có 2 túi trước nhiều màu mặc mùa đông ấm ấp", "68.000đ" )
+}
+function btn2() {
+    localStorage.setItem("Giày Thể Thao Đế Dày Phối Lưới Thoáng Khí Phong Cách Hàn Quốc Thời Trang Mùa Hè Mới TYT106", "92.600đ" )
+}
+function btn3() {
+    localStorage.setItem("Set Bộ Đồ Loang Màu Nam Áo Thun Ngắn Tay Kèm Quần Jogger Thời Trang Zenkocs3 MEN TOP 221 + QUAN 069", "35.000đ" )
+}
+function btn4() {
+    localStorage.setItem("ComBo Áo Thun Nam Ngắn Tay TUNNEL Kết Hợp Quần Jogger Trẻ Trung Thời Trang STYLE MARVEN - SET NAM", "89.100đ")
+}
+function btn5() {
+    localStorage.setItem("Giày Sneaker Nam Phong Cách Cực Ngầu Thời Trang Trẻ Trung", "268.000đ")
+}
+function btn6() {
+    localStorage.setItem("Giày cv cổ cao full box hình gấu BEARBRICK, giày thể thao bata học sinh vải canvas đế bằng S35", "141.000đ" )
+}
+
+
